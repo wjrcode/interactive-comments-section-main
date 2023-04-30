d172e2bc9defc9dea6c6073b67403a8b569ebf5f
wjrcode <willianjunior012@gmail.com>
2023-04-30 09:26:41 -0300

    the start

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <template v-for="(comment, i) in comments" :key="`item-${i}`">
+      <CommentCard v-bind="i" :name="comment.user.username" :photo="comment.user.image.png" :comment="comment.content" />
+    </template>
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CommentCard from './components/CommentCard.vue';
+import data from './data/data.json'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CommentCard
+  },
+  computed: {
+    pic() {
+      return '/images/avatars/image-amyrobson.png'
+    }
+  },
+  data: function () {
+    return {
+      comments: data.comments,
+      imgPath: './images/avatars/image-amyrobson.png'
+    }
+  },
+  beforeMount() {
+    window.console.log(data.comments)
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap');
+
+.v-application {
+  font-family: "Space Mono";
+}
+
+html {
+  font-family: "Space Mono";
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100%;
+}
+
+body {
+  margin: 0;
+  background-color: hsl(185, 41%, 84%);
+}
+
+.attribution {
+  font-size: 11px;
+  text-align: center;
+  margin-top: 1rem;
+}
+
+.attribution a {
+  color: hsl(228, 45%, 44%);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/CommentCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <div class="card__vote-button"></div>
+    <div class="card__content">
+      <div class="card__header">
+        <div class="card__details">
+          <div class="card__profile-photo">
+          
+
+            <img :src="photo"/>
+          </div>
+          <div class="card__user-name">
+            {{ name }}
+          </div>
+          <div class="card__time">
+            {{ time }}
+          </div>
+        </div>
+        <div class="card__reply-button"></div>
+      </div>
+      <div class="card__comment">
+        {{ comment }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommentCard',
+  props: {
+    photo: {type: String},
+    name: String,
+    time: String,
+    comment: String
+  },
+  computed:{
+    pic(){
+      return require(this.photo)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {}
+</style>
